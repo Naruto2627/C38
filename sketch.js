2edd0a98f753976ee91e9e99cef079c17aa805b5
Naruto2627 <venkateshsuvarna26@gmail.com>
2020-08-12 20:28:52 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -8,6 +8,15 @@ var database;
 var form, player, game;
 var cars,car1,car2,car3,car4;
 
+function preload(){
+  track = loadImage("images/track.png");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+}
+
 function setup(){
   canvas = createCanvas(displayWidth-50,displayHeight-180);
   database = firebase.database();
@@ -24,5 +33,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
